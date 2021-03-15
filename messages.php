ea968ef7c89f7cda54590e16da01cce5a90502b1
quyen <qh8ar@virginia.edu>
2021-03-15 12:57:30 -0400

    alex's msgs

--- messages.php
+++ messages.php
@@ -12,7 +12,7 @@
   <body>
     <?php include('navbar.html') ?>
 
-    <div class="border">
+    <!-- <div class="border">
       <div class="sidebar">
         <div class="msg-name">Name</div>
         <div class="msg-preview">Message Preview</div>
@@ -22,7 +22,59 @@
         <h5>hi</h5>
         <p>hsdkf</p>
       </div>
-    </div>
+    </div> -->
+
+    <div class="row">
+        <div class="col">
+            <div class="col">
+                <div class="box">
+                    <p style="">Jane Doe</p>
+                    <p style="font-size: 13px;">Preview Message</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box">
+                    <p style="">John Doe</p>
+                    <p style="font-size: 13px;">Preview Message</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box">
+                    <p style="">Bob Doe</p>
+                    <p style="font-size: 13px;">Preview Message</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box">
+                    <p style="">Alex Doe</p>
+                    <p style="font-size: 13px;">Preview Message</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="box">
+                    <p style="">Quin Doe</p>
+                    <p style="font-size: 13px;">Preview Message</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="box2">
+                <img style="display:inline-block;" src="media/profile-picture.jpg" class="profile-pic3" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" alt="">
+                <p style="display:inline-block; font-weight: bold; padding: 20px;">Jane Doe</p>
+            </div>
+            <br></br>
+            <div class="messages">
+                <p>Hello, this is a test message </p>
+            </div>
+            <br></br>
+            <div style="float: right;" class="messages">
+                <p>Hellow, this is a test message</p>
+            </div>
+            <div class="input-group">
+                <textarea class="form-control custom-control" rows="3" style="resize:none"></textarea>     
+                <span class="input-group-addon btn btn-primary">Send</span>
+            </div>
+      </div>
 
     <?php include('js.html') ?>
   </body>

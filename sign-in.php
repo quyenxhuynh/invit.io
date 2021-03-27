8fff1f6ab197ac3ed83eee2a73fb66d1fea50b94
quyen <qh8ar@virginia.edu>
2021-03-27 16:26:14 -0400

    log in

--- sign-in.php
+++ sign-in.php
@@ -1,4 +1,41 @@
-<!-- BASE TEMPLATE TO C/P TO NEW PAGES -->
+<?php
+
+session_start();
+if (isset($_SESSION['logged-in'])) {
+	header('Location: base.php');
+}
+
+include_once("./config.php");
+$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+if (isset($_POST['sign-in'])) {
+	$sql = "SELECT password FROM User WHERE username = '$_POST[username]'";
+	$rs = mysqli_query($con, $sql);
+	if (!$rs) {
+		echo mysqli_error($con);
+	}
+	if (mysqli_num_rows($rs) < 1) {
+		$error = "Username doesn't exist";
+	} else {
+		$row = mysqli_fetch_array($rs, MYSQLI_ASSOC);
+
+		if(password_verify($_POST['password'], $row['password'])){
+			$error =  "";
+			$_SESSION['logged-in'] = $_POST['username'];
+			header("Location: base.php");
+		} else {
+			$error = "The username or password do not match";
+		}
+	}
+	mysqli_close($con);
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -14,18 +51,17 @@
 <body>
 	<?php include('navbar.html') ?>
 
-	<?php 
-	if (isset($error)) {
-		echo '<div class="alert alert-warning" role="alert">' . 
-			$error . 
-			'</div>';
-	}
-	?>
-
 	<div class="container">
-		<h2>Create a New Account</h2>
-		<p>
-		<form action="sign-in-action.php" method="post">
+		<?php 
+		if (isset($error)) {
+			echo '<div class="alert alert-danger" role="alert">' . 
+				$error . 
+				'</div>';
+		}
+		?>
+		<h2>Sign in</h2>
+		<br>
+		<form method="post">
 			<div class="form-group row">
 				<label class="col-sm-2 col-form-label">Username</label>
 				<div class="col-sm-10">
@@ -38,9 +74,9 @@
 					<input id="password" name="password" class="form-control" type="password" placeholder="********" required>
 				</div>
 			</div>
-			<button class="btn-blue-muted float-right px-4" type="submit">Sign In</button>
+			<button name="sign-in" class="btn-blue-muted float-right px-4" type="submit">Sign In</button>
 		</form>
-		</p>
+		<small>Don't have an account? <a href="sign-up.php">Sign up!</a></small>
 	</div>
 
 	<?php include('js.html') ?>

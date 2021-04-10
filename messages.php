b79e1d14de2d84ed6e1cb2feecd0830126020307
Quyen Huynh <qh8ar@virginia.edu>
2021-04-10 07:16:57 -0400

    email vers of msgs for assignment 3

--- messages.php
+++ messages.php
@@ -16,6 +16,7 @@ if (!isset($_SESSION['logged_in'])) {
 
 	<?php include("styles.html") ?>
 	<link rel="stylesheet" href="/invit.io/css/messages.css">
+	<!-- <link rel="stylesheet" href="/invit.io/css/test.css"> -->
 
 	<title>invit.io</title>
 </head>
@@ -25,34 +26,57 @@ if (!isset($_SESSION['logged_in'])) {
 
 	<div class="border">
 		<div class='sidebar'>
-			<div class='msg-side'>
-				<div class='msg-name'>John Doe</div>
-				<div class='msg-preview'>hello</div>
+			<div class='search'>
+				<a href="new-msg.php" class='btn-blue-muted'>+ New Message</a>
 			</div>
+			<div class='msg-prev'>
+				<?php
+				$sql = "SELECT * FROM Msg WHERE to_user = ? ORDER BY time DESC LIMIT 15";
+				$stmt = $con->prepare($sql);
+				$stmt->bind_param('s', $_SESSION['logged_in']);
+				$stmt->execute();
+				$rs = $stmt->get_result();
 
-			<div class='hl'></div>
+				if ($rs->num_rows > 0) {
+					$i = 0;
+					while ($row = $rs->fetch_assoc()) {
+						echo '<h6><a href="messages.php?id=' . $row['msg_id'] . '">';
+							echo $row['from_user'] . "<br>";
+							echo '<div class="msg-preview">';
+							echo substr($row['msg_content'], 0, min(strlen($row['msg_content']), 32));
+							echo '</div>';
+							echo '</a></h6>';
+						if ($i != $rs->num_rows-1) {
+							echo '<div class="hl"></div>';
+						}
+						$i += 1;
+					}
+				}
+				?>
+			</div>
 		</div>
-		<div class='vl'></div>
-		<div class='current-msg'>
+		<div class='cur-msg'>
 			<div class='header-row'>
-				<img src="./upload/default.jpg" alt="profile picture" class="profile-pic" width=100px>
-				<h4 class='m-2'>John Doe</h4>
-			</div>
-			<div class='hl'></div>
-			<div class='msgs'>
-				<div class='prev-msgs'>
-					<div class='to-msg'>hj!</div>
-					<div class='from-msg'>hello!</div>
-				</div>
-				<div class='new-msg'>
-					<form action="">
-						<input size='1' type="text" name="" class='new-msg-content' placeholder="Message">
-						<button class='new-msg-btn'>Send</button>
-					</form>
-				</div>
+				<img src="/invit.io/upload/default.jpg" class="profile-pic" width=100px>
+				
+				<?php
+				if (!empty($_GET)) {
+					$sql = "SELECT * FROM Msg WHERE msg_id=?";
+					$stmt = $con->prepare($sql);
+					$num = (int)$_GET['id'];
+					$stmt->bind_param('i', $num);
+					$stmt->execute();
+					$rs = $stmt->get_result();
+					$row = $rs->fetch_assoc();
 
+					echo "<h3 class='m-2'>" . $row['from_user'] . '</h3>';
+					echo '</div>';
+					echo "<div class='msg-content'>";
+					echo $row['msg_content'];
+					echo '</div>';
+				}
+				?>
 			</div>
-
 		</div>
 	</div>
 
